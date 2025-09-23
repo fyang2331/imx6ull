bf045ead495b56ca5ea275f3b4f0b1b56ba0166f
desperado <d1216174090@126.com>
2025-09-24 00:42:56 +0800

    修复编译没有产物uboot.bin问题

--- mx6ull_alientek_emmc.sh
+++ mx6ull_alientek_emmc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alientek_emmc_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j16
+make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- u-boot.bin
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- u-boot.imx
