293e9027e919ef6375d7e5fab88386c989218195
desperado <d1216174090@126.com>
2025-09-24 00:46:15 +0800

    精简编译脚本

--- mx6ull_alientek_emmc.sh
+++ mx6ull_alientek_emmc.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alientek_emmc_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- u-boot.bin
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- u-boot.imx
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12 all u-boot.bin u-boot.imx
