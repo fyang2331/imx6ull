2e833424d17d9a16dacf52ef8923377ad8b28ea9
yf <d1216174090@126.com>
2026-03-13 13:43:05 +0800

    删除不必要文件

--- mx6ull_alientek_emmc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_alientek_emmc_defconfig
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j12 all u-boot.bin u-boot.imx
